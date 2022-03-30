9acd8e9fcd7c596a6ba0dcf67386c4d55052d4f1
kfung15 <kennardfung01@gmail.com>
2022-03-30 15:42:59 +0800

    added form on right side

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,59 @@
-import React from 'react';
+import {useState} from 'react'
 import './Card.css'
+import {Container, Row, Col, Form, FormLabel} from 'react-bootstrap'
+import ForceGraphMaker from './forceGraphMaker'
 
 
 
 function Card (props) {
+    const [region, setRegion] = useState([])
+    const [country, setCountry] = useState(["Singapore"])
+    const [rating, setRating] = useState(["AA+"])
+    const [sector, setSector] = useState(["govt"])
+    const [ticker, setTicker] = useState(["SGGOV5Y", "SGGOV2Y", "SGGOV1Y"])
+    const [maturity, setMaturity] = useState(["5y","2y","1y"])
 
+    console.log(region)
+    let x = ForceGraphMaker({"region": region, "country": country, "rating": rating, "sector": sector, "ticker": ticker, "maturity": maturity})
+    console.log(x)
     return (
-            <div class="card">
-                {props.contentObj}
-            </div>
+        <div>
+            <Container fluid>
+                <Row>
+                    <Col xs={7}>
+                        {x}
+                    </Col>
+                    <Col>
+                        <Form>
+                            <FormLabel>
+                                Region
+                            </FormLabel>
+                            <Form.Control type="text" placeholder={region} onChange={e => setRegion(e.target.value)} />
+                            <FormLabel>
+                                Country
+                            </FormLabel>
+                            <Form.Control type="text" placeholder={country} onChange={e => setCountry(e.target.value)}/>
+                            <FormLabel>
+                                Rating
+                            </FormLabel>
+                            <Form.Control type="text" placeholder={rating} onChange={e => setRating(e.target.value)}/>
+                            <FormLabel>
+                                Sector
+                            </FormLabel>
+                            <Form.Control type="text" placeholder={sector} onChange={e => setSector(e.target.value)}/>
+                            <FormLabel>
+                                Ticker
+                            </FormLabel>
+                            <Form.Control type="text" placeholder={ticker} onChange={e => setTicker(e.target.value)}/>
+                            <FormLabel>
+                                Maturity
+                            </FormLabel>
+                            <Form.Control type="text" placeholder={maturity} onChange={e => setMaturity(e.target.value)}/>      
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>          
+        </div>
     )
 
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
 
 
 ReactDOM.render(
