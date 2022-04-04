47abff2e40d87d22aa660ccacd2313c67a8635e6
kfung15 <kennardfung01@gmail.com>
2022-04-04 09:49:34 +0800

    implemented "All" filtering

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import './Card.css'
-import {Container, Row, Col, Form, FormLabel} from 'react-bootstrap'
+import {Container, Row, Col, Form, FormLabel, Dropdown} from 'react-bootstrap'
 import ForceGraphMaker from './forceGraphMaker'
 
 
@@ -9,47 +9,88 @@ import ForceGraphMaker from './forceGraphMaker'
 
 function Card () {
 
-    const [region, setRegion] = useState(["Asia"])
-    const [country, setCountry] = useState(["Singapore"])
-    const [rating, setRating] = useState(["AA+"])
-    const [sector, setSector] = useState(["govt"])
+    const [region, setRegion] = useState(["All"])
+    const [country, setCountry] = useState(["All"])
+    const [rating, setRating] = useState(["All"])
+    const [sector, setSector] = useState(["All"])
     const [ticker, setTicker] = useState(["SGGOV5Y", "SGGOV2Y", "SGGOV1Y"])
     const [maturity, setMaturity] = useState(["5y","2y","1y"])
     
 
     return (
         <div>
+            <br />
             <Container fluid>
                 <Row>
                     <Col xs={7}>
-                        <ForceGraphMaker content={{region, country, rating, sector, ticker, maturity}}/>
+                        <ForceGraphMaker content={{region, country, rating, sector}}/>
                     </Col>
                     <Col>
                         <Form>
                             <FormLabel>
                                 Region
                             </FormLabel>
-                            <Form.Control type="text" placeholder={region} onChange={e => setRegion(e.target.value)}/>
+                            <Dropdown>
+                                <Dropdown.Toggle>
+                                    {region}
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item onClick={e => setRegion("All")}>All</Dropdown.Item>
+                                    <Dropdown.Item onClick={e => setRegion("Asia")}>Asia</Dropdown.Item>
+                                    <Dropdown.Item onClick={e => setRegion("Africa")}>Africa</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                            {/* <Form.Control type="text" placeholder={region} onChange={e => setRegion(e.target.value)}/> */}
                             <FormLabel>
                                 Country
                             </FormLabel>
-                            <Form.Control type="text" placeholder={country} onChange={e => setCountry(e.target.value)}/>
+                            <Dropdown>
+                                <Dropdown.Toggle>
+                                    {country}
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item onClick={e => setCountry("All")}>All</Dropdown.Item>
+                                    <Dropdown.Item onClick={e => setCountry("Singapore")}>Singapore</Dropdown.Item>
+                                    <Dropdown.Item onClick={e => setCountry("USA")}>USA</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
                             <FormLabel>
                                 Rating
                             </FormLabel>
-                            <Form.Control type="text" placeholder={rating} onChange={e => setRating(e.target.value)}/>
+                            <Dropdown>
+                                <Dropdown.Toggle>
+                                    {rating}
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item onClick={e => setRating("All")}>All</Dropdown.Item>
+                                    <Dropdown.Item onClick={e => setRating("AA+")}>AA+</Dropdown.Item>
+                                    <Dropdown.Item onClick={e => setRating("AA")}>AA</Dropdown.Item>
+                                    <Dropdown.Item onClick={e => setRating("A")}>A</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                            {/* <Form.Control type="text" placeholder={rating} onChange={e => setRating(e.target.value)}/> */}
                             <FormLabel>
                                 Sector
                             </FormLabel>
-                            <Form.Control type="text" placeholder={sector} onChange={e => setSector(e.target.value)}/>
-                            <FormLabel>
+                            <Dropdown>
+                                <Dropdown.Toggle>
+                                    {sector}
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item onClick={e => setSector("All")}>All</Dropdown.Item>
+                                    <Dropdown.Item onClick={e => setSector("govt")}>govt</Dropdown.Item>
+                                    <Dropdown.Item onClick={e => setSector("energy")}>energy</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                            {/* <Form.Control type="text" placeholder={sector} onChange={e => setSector(e.target.value)}/> */}
+                            {/* <FormLabel>
                                 Ticker
                             </FormLabel>
                             <Form.Control type="text" placeholder={ticker} onChange={e => setTicker(e.target.value)}/>
                             <FormLabel>
                                 Maturity
                             </FormLabel>
-                            <Form.Control type="text" placeholder={maturity} onChange={e => setMaturity(e.target.value)}/>      
+                            <Form.Control type="text" placeholder={maturity} onChange={e => setMaturity(e.target.value)}/>       */}
                         </Form>
                     </Col>
                 </Row>
--- src/components/forceGraphMaker.js
+++ src/components/forceGraphMaker.js
@@ -22,8 +22,13 @@ function ForceGraphMaker(criteria) {
       let bond_data = {"nodes" : [], "links": []}
       console.log(JSON.stringify(masterBondData))
       console.log(criteria)
+
+      // Go through the filtering process
+      bond_data.nodes = criteria.content.region != "All" ? masterBondData.nodes.filter(node => criteria.content.region.includes(node.region)) : masterBondData.nodes
+      bond_data.nodes = criteria.content.country != "All" ? bond_data.nodes.filter(node => criteria.content.country.includes(node.country)) : bond_data.nodes
+      bond_data.nodes = criteria.content.rating != "All" ? bond_data.nodes.filter(node => criteria.content.rating === node.rating) : bond_data.nodes
+      bond_data.nodes = criteria.content.sector != "All" ? bond_data.nodes.filter(node => criteria.content.sector.includes(node.sector)) : bond_data.nodes
       
-      bond_data.nodes = masterBondData.nodes.filter(node => criteria.content.region.includes(node.region) & criteria.content.country.includes(node.country) & criteria.content.rating.includes(node.rating) & criteria.content.sector.includes(node.sector) & criteria.content.ticker.includes(node.ticker) & criteria.content.maturity.includes(node.maturity))
       console.log(bond_data.nodes)
 
       let bondIds = []
